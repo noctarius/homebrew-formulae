4579abdb387563f2f7520b27e20d840527ef161c
Christoph Engelbert <me@noctarius.com>
2018-10-16 19:52:23 +0200

    Updated release version of wordpress-sanitizer to 0.9.5

--- Formula/wordpress-sanitizer.rb
+++ Formula/wordpress-sanitizer.rb
@@ -1,7 +1,7 @@
 class WordpressSanitizer < Formula
   desc "Sanitizes HTML based on rules sets written in JavaScript"
   homepage "https://github.com/noctarius/wordpress-sanitizer"
-  url "https://github.com/noctarius/wordpress-sanitizer.git", :tag => "0.9.3"
+  url "https://github.com/noctarius/wordpress-sanitizer.git", :tag => "0.9.5"
   sha256 "3fc18dd51d94390b035c602b92d3877f056fd11842f68172ae6a928f6480d68e"
 
   depends_on "go" => :build
